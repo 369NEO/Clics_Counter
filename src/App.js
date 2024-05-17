b2690bf56609da25199be56c368b0219d5ddfa54
369NEO <42665320+369NEO@users.noreply.github.com>
2024-05-17 16:54:39 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import Boton from "./componentes/boton";
+import freecodecamp from "./imagenes/freecodecamp.png";
+import Contador from "./componentes/Contador";
+import { useState } from "react";
+
+export default function App() {
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  };
+  const reiniciarContador = () => {
+    setNumClics(0);
+  };
+
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          className="freecodecamp-logo"
+          src={freecodecamp}
+          alt="Logo de freeCodeCamp"
+        />
+      </div>
+      <div className="contenedor-principal">
+        <Contador numClics={numClics} />
+        <Boton texto="Click" esBotonDeClic={true} manejarClic={manejarClic} />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
+      </div>
+    </div>
+  );
+}
